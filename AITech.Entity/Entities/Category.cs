b84e6adb36a1cdcbb4fa62d70eb790c15554e2d1
Sinan Kaya <kayaasinan@gmail.com>
2025-11-18 22:34:56 +0300

    Add project files.

---
+++ AITech.Entity/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using AITech.Entity.Entities.Common;
+using System.Reflection.Metadata;
+
+namespace AITech.Entity.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public IList<Project> Projects { get; set; }
+    }
+}
